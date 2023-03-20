d956809f510af0f8973227d47fa23cdd523e3cfe
hdXian <hyun353500@naver.com>
2023-03-21 00:54:00 +0900

    C230321

---
+++ src/main/java/hdxian/hdxianspring/HdxianSpringApplication.java
@@ -0,0 +1,13 @@
+package hdxian.hdxianspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdxianSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdxianSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hdxian/hdxianspring/HdxianSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hdxian.hdxianspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdxianSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
