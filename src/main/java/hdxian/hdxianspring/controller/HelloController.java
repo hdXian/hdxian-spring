7bbcc5427b0cb91f53436385a38be179aada53be
hdXian <hyun353500@naver.com>
2023-04-16 00:41:47 +0900

    c230416

--- src/main/java/hdxian/hdxianspring/controller/HelloController.java
+++ src/main/java/hdxian/hdxianspring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hdxian.hdxianspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,41 @@ public class HelloController {
         model.addAttribute("data", "suction~");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello h1 = new Hello();
+        h1.setName(name);
+        return h1;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
+
 
 }
---
+++ src/main/java/hdxian/hdxianspring/service/MemberService.java
@@ -0,0 +1,48 @@
+package hdxian.hdxianspring.service;
+
+import hdxian.hdxianspring.domain.Member;
+import hdxian.hdxianspring.repository.MemberRepository;
+import hdxian.hdxianspring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public class MemberService {
+
+    // private final MemberRepository repository = new MemoryMemberRepository();
+    private final MemberRepository repository;
+
+    public MemberService(MemberRepository repository) {
+        this.repository = repository;
+    }
+
+    // 회원 가입 기능
+    public Long join(Member member) {
+
+        // 중복된 이름으로는 가입 불가.
+        checkNameDuplication(member);
+
+        repository.save(member);
+        return member.getId();
+    }
+
+    private void checkNameDuplication(Member member) {
+        repository.findByName(member.getName()) // findbyName()의 리턴 타입은 Optional<Member>
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return repository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return repository.findById(memberId);
+    }
+
+
+}
